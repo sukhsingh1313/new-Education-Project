515305b5798c4eb4d99c140c26cf04ecc383d0fe
Sukhchain Singh <sukhsingh1566@gmail.com>
2025-03-01 18:59:43 +0530

    Updated project files

--- Education-project/schoolProject/schoolProject/settings.py
+++ Education-project/schoolProject/schoolProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lb3^zyvo@nihj9v-bgh4+!riukz-1-qkn(eik+nos0peghi7$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # custom add 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,10 +80,12 @@ WSGI_APPLICATION = 'schoolProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+
 }
 
 
@@ -125,6 +128,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
